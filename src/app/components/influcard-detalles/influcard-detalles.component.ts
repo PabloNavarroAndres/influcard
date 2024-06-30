a225e49875b13399ffb364a049c63045464c094a
PabloNavarroAndres <pablo.navarro.andres01@gmail.com>
2024-06-30 11:27:56 +0200

    implementado amchart distribucion por genero

--- src/app/components/influcard-detalles/influcard-detalles.component.ts
+++ src/app/components/influcard-detalles/influcard-detalles.component.ts
@@ -4,6 +4,16 @@ import { Influcard } from '../../models/influcard-model';
 import { InflucardService } from '../../services/influcard.service';
 import { CommonModule } from '@angular/common';
 
+import { Inject, NgZone, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+// amCharts imports
+import * as am5xy from '@amcharts/amcharts5/xy';
+import * as am5 from "@amcharts/amcharts5";
+import * as am5percent from "@amcharts/amcharts5/percent";
+import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
+
+
 @Component({
   selector: 'app-influcard-detalles',
   standalone: true,
@@ -17,6 +27,10 @@ export class InflucardDetallesComponent {
 
   private _route = inject(ActivatedRoute);
   private _influcardService = inject(InflucardService);
+  
+  private root!: am5.Root;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object, private zone: NgZone) {}
  
   // Porcentaje Reach formateado para el svg
   get reachPercentage(): string {
@@ -52,10 +66,86 @@ export class InflucardDetallesComponent {
         console.error('Error obteniendo datos de Influcard:', err);
       }
     });
+
   }
 
   descargarInflucard(): void {
     /* ... */
+  }
+
+   // Run the function only in the browser
+   browserOnly(f: () => void) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.zone.runOutsideAngular(() => {
+        f();
+      });
+    }
+  }
+
+  ngAfterViewInit() {
+    // Chart code goes in here
+    this.browserOnly(() => {
+
+      // Create root element
+      // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+      var root = am5.Root.new("chartdiv");
+
+
+      // Set themes
+      // https://www.amcharts.com/docs/v5/concepts/themes/
+      root.setThemes([
+        am5themes_Animated.new(root)
+      ]);
+
+
+      // Create chart
+      // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/
+      var chart = root.container.children.push(am5percent.PieChart.new(root, {
+        layout: root.verticalLayout
+      }));
+
+
+      // Create series
+      // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Series
+      var series = chart.series.push(am5percent.PieSeries.new(root, {
+        valueField: "value",
+        categoryField: "category"
+      }));
+
+
+      // Set data
+      // https://www.amcharts.com/docs/v5/charts/percent-charts/pie-chart/#Setting_data
+      series.data.setAll([
+        { value: this.influcard?.insight_perc_m, category: "Hombres" },
+        { value: this.influcard?.insight_perc_f, category: "Mujeres" }
+      ]);
+
+
+      // Create legend
+      // https://www.amcharts.com/docs/v5/charts/percent-charts/legend-percent-series/
+      var legend = chart.children.push(am5.Legend.new(root, {
+        centerX: am5.percent(50),
+        x: am5.percent(50),
+        marginTop: 15,
+        marginBottom: 15
+      }));
+
+      legend.data.setAll(series.dataItems);
+
+
+      // Play initial series animation
+      // https://www.amcharts.com/docs/v5/concepts/animations/#Animation_of_series
+      series.appear(1000, 100);
+    });
+  }
+
+  ngOnDestroy() {
+    // Clean up chart when the component is removed
+    this.browserOnly(() => {
+      if (this.root) {
+        this.root.dispose();
+      }
+    });
   }
 
 }

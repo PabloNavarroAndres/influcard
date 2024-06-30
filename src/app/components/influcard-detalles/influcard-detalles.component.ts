747506d7b31fad93ba35d8ae253ff4fde7b29cfe
PabloNavarroAndres <pablo.navarro.andres01@gmail.com>
2024-06-30 14:10:49 +0200

    amchart franja horaria de sus publicaciones implementado

--- src/app/components/influcard-detalles/influcard-detalles.component.ts
+++ src/app/components/influcard-detalles/influcard-detalles.component.ts
@@ -102,7 +102,7 @@ export class InflucardDetallesComponent {
 
 
         /* Amchart 3: Franja horaria publicaciones */
-        /* ... */
+        this.createPostsTimeSlotChart();
 
 
         /* Amchart 4: Engagement posts por dia de publicacion */
@@ -361,8 +361,237 @@ export class InflucardDetallesComponent {
   /* Amchart 3: Franja horaria de sus publicaciones */
   private createPostsTimeSlotChart() {
 
-    
+    // Create root element
+    // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+    let root = am5.Root.new("chartdiv3");
 
+    // Set themes
+    // https://www.amcharts.com/docs/v5/concepts/themes/
+    root.setThemes([
+      am5themes_Animated.new(root)
+    ]);
+
+    // Valores franja horaria en influcard
+    let morning = Number(this.influcard!.account_post_moment[0].total);
+    let afternoon = Number(this.influcard!.account_post_moment[1].total);
+    let night = Number(this.influcard!.account_post_moment[2].total);
+
+    let data = [
+      {
+        name: "Noche",
+        steps: night,
+        pictureSettings: {
+          src: "https://fontawesome.com/icons/moon?f=classic&s=solid"
+        }
+      },
+      {
+        name: "Tarde",
+        steps: afternoon,
+        pictureSettings: {
+          src: "https://fontawesome.com/icons/cloud-sun?f=classic&s=solid"
+        }
+      },
+      {
+        name: "Mañana",
+        steps: morning,
+        pictureSettings: {
+          src: "https://fontawesome.com/icons/sun?f=classic&s=solid"
+        }
+      }
+    ];
+
+    // Create chart
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/
+    let chart = root.container.children.push(
+      am5xy.XYChart.new(root, {
+        panX: false,
+        panY: false,
+        paddingLeft:0,
+        paddingRight:30,
+        wheelX: "none",
+        wheelY: "none"
+      })
+    );
+
+    // Create axes
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+
+    let yRenderer = am5xy.AxisRendererY.new(root, {
+      minorGridEnabled:true
+    });
+    yRenderer.grid.template.set("visible", false);
+
+    let yAxis = chart.yAxes.push(
+      am5xy.CategoryAxis.new(root, {
+        categoryField: "name",
+        renderer: yRenderer,
+        paddingRight:40
+      })
+    );
+
+    let xRenderer = am5xy.AxisRendererX.new(root, {
+      minGridDistance:80,
+      minorGridEnabled:true
+    });
+
+    let xAxis = chart.xAxes.push(
+      am5xy.ValueAxis.new(root, {
+        min: 0,
+        renderer: xRenderer
+      })
+    );
+
+    // Add series
+    // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+    let series = chart.series.push(
+      am5xy.ColumnSeries.new(root, {
+        name: "Income",
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueXField: "steps",
+        categoryYField: "name",
+        sequencedInterpolation: true,
+        calculateAggregates: true,
+        maskBullets: false,
+        tooltip: am5.Tooltip.new(root, {
+          dy: -30,
+          pointerOrientation: "vertical",
+          labelText: "{valueX}"
+        })
+      })
+    );
+
+    series.columns.template.setAll({
+      strokeOpacity: 0,
+      cornerRadiusBR: 10,
+      cornerRadiusTR: 10,
+      cornerRadiusBL: 10,
+      cornerRadiusTL: 10,
+      maxHeight: 20,
+      fillOpacity: 0.8
+    });
+
+    let currentlyHovered: any;
+
+    series.columns.template.events.on("pointerover", function(e) {
+      handleHover(e.target.dataItem);
+    });
+
+    series.columns.template.events.on("pointerout", function(e) {
+      handleOut();
+    });
+
+    function handleHover(dataItem: any) {
+      if (dataItem && currentlyHovered != dataItem) {
+        handleOut();
+        currentlyHovered = dataItem;
+        let bullet = dataItem.bullets[0];
+        bullet.animate({
+          key: "locationX",
+          to: 1,
+          duration: 600,
+          easing: am5.ease.out(am5.ease.cubic)
+        });
+      }
+    }
+
+    function handleOut() {
+      if (currentlyHovered) {
+        let bullet = currentlyHovered.bullets[0];
+        bullet.animate({
+          key: "locationX",
+          to: 0,
+          duration: 600,
+          easing: am5.ease.out(am5.ease.cubic)
+        });
+      }
+    }
+
+
+    let circleTemplate: any = am5.Template.new({});
+
+    series.bullets.push(function(root, series, dataItem) {
+      let bulletContainer = am5.Container.new(root, {});
+      let circle = bulletContainer.children.push(
+        am5.Circle.new(
+          root,
+          {
+            radius: 24
+          },
+          circleTemplate
+        )
+      );
+
+      let maskCircle = bulletContainer.children.push(
+        am5.Circle.new(root, { radius: 27 })
+      );
+
+      // only containers can be masked, so we add image to another container
+      let imageContainer = bulletContainer.children.push(
+        am5.Container.new(root, {
+          mask: maskCircle
+        })
+      );
+
+      // not working
+      let image = imageContainer.children.push(
+        am5.Picture.new(root, {
+          templateField: "pictureSettings",
+          centerX: am5.p50,
+          centerY: am5.p50,
+          width: 30,
+          height: 30
+        })
+      );
+
+      return am5.Bullet.new(root, {
+        locationX: 0,
+        sprite: bulletContainer
+      });
+    });
+
+    // heatrule
+    series.set("heatRules", [
+      {
+        dataField: "valueX",
+        min: am5.color(0xe5dc36),
+        max: am5.color(0x5faa46),
+        target: series.columns.template,
+        key: "fill"
+      },
+      {
+        dataField: "valueX",
+        min: am5.color(0xe5dc36),
+        max: am5.color(0x5faa46),
+        target: circleTemplate,
+        key: "fill"
+      }
+    ]);
+
+    series.data.setAll(data);
+    yAxis.data.setAll(data);
+
+    let cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
+    cursor.lineX.set("visible", false);
+    cursor.lineY.set("visible", false);
+
+    cursor.events.on("cursormoved", function() {
+      let dataItem = series.get("tooltip")?.dataItem;
+      if (dataItem) {
+        handleHover(dataItem)
+      }
+      else {
+        handleOut();
+      }
+    })
+
+    // Make stuff animate on load
+    // https://www.amcharts.com/docs/v5/concepts/animations/
+    series.appear();
+    chart.appear(1000, 100);
+    
+    /* Añadir chart al array de elementos amchart */
+    this.roots.push(root);
   }
 
   /* Amchart 4: Engagement segun dia de publicacion */
